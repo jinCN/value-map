19b0464ab487c48b142e5f1ab242cf44e8d91e40
liangjunren <liangjunren@bytedance.com>
2019-05-10 12:11:53 +0800

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# value-map
+extended ES6 Map using object as key
---
+++ index.js
@@ -0,0 +1,44 @@
+const stringify = require('fast-json-stable-stringify')
+
+function guardStringify (key) {
+  if (key && typeof key === 'object') {
+  } else {
+    throw new Error('[value-map] key must be object')
+  }
+  return stringify(key)
+}
+
+class ValueMap extends Map {
+  * entries () {
+    for (let [k, v] of super.entries()) {
+      yield [JSON.parse(k), v]
+    }
+  }
+
+  forEach (callback, thisArg) {
+    for (let [k, v] of this) {
+      callback.call(thisArg, v, k, this)
+    }
+  }
+
+  * keys () {
+    for (let k of super.keys()) {
+      yield JSON.parse(k)
+    }
+  }
+
+  has (key) {
+    return super.has(guardStringify(key))
+  }
+
+  delete (key) {
+    return super.delete(guardStringify(key))
+  }
+
+  set (key, value) {
+    return super.set(guardStringify(key), value)
+  }
+}
+
+ValueMap.prototype[Symbol.iterator] = ValueMap.prototype.entries
+module.exports = ValueMap
